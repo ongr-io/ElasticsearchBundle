960b08b35206c9a312c7f9c89f4af6b925851aaa
Martynas Sudintas <martynas.sudintas@ongr.io>
2015-02-27 09:17:34 +0200

    Removed unused namespaces

--- DataCollector/ElasticsearchDataCollector.php
+++ DataCollector/ElasticsearchDataCollector.php
@@ -12,7 +12,6 @@
 namespace ONGR\ElasticsearchBundle\DataCollector;
 
 use Monolog\Logger;
-use ONGR\ElasticsearchBundle\Common\JsonFormatter;
 use ONGR\ElasticsearchBundle\Logger\Handler\CollectionHandler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
