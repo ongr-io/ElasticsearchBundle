e9157d4e32b17a43c92c6903fb4269fc7a764df2
Linas Mockus <linas.mockus@ongr.io>
2015-02-27 11:21:42 +0200

    Posibility to configure ES manager as readonly.

--- DependencyInjection/Compiler/MappingPass.php
+++ DependencyInjection/Compiler/MappingPass.php
@@ -147,6 +147,8 @@ class MappingPass implements CompilerPassInterface
             ]
         );
 
+        $connection->addMethodCall('setReadOnly', [$settings['readonly']]);
+
         $this->setWarmers($connection, $settings['connection'], $container);
 
         return $connection;
