60f7cf11d53a648e058a27e41d03239966852a89
Andrius Bil <andrius.bilinevicius@nfq.lt>
2015-03-09 13:40:04 +0200

    Additional tests for ElasticsearchExtension class

--- Tests/Unit/DependencyInjection/ElasticsearchExtensionTest.php
+++ Tests/Unit/DependencyInjection/ElasticsearchExtensionTest.php
@@ -12,7 +12,11 @@
 namespace ONGR\ElasticsearchBundle\Tests\Unit\DependencyInjection;
 
 use ONGR\ElasticsearchBundle\DependencyInjection\ONGRElasticsearchExtension;
+use Symfony\Component\Config\Resource\DirectoryResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Unit tests for ElasticsearchExtension.
@@ -20,34 +24,26 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class ElasticsearchExtensionTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Check if load adds parameters to container as expected.
+     * @return array
      */
-    public function testLoad()
+    public function getData()
     {
-        $container = new ContainerBuilder();
-        $container->setParameter('kernel.bundles', []);
-        $container->setParameter('kernel.cache_dir', '');
-        $container->setParameter('kernel.debug', true);
-        $extension = new ONGRElasticsearchExtension();
-        $extension->load(
-            [
-                'elasticsearch' => [
-                    'document_dir' => 'customDir',
-                    'connections' => [
-                        'test' => ['index_name' => 'test'],
-                    ],
-                    'managers' => [
-                        'test' => [
-                            'connection' => 'test2',
-                            'debug' => false,
-                            'readonly' => false,
-                            'mappings' => ['testBundle'],
-                        ],
+        $parameters = [
+            'elasticsearch' => [
+                'document_dir' => 'customDir',
+                'connections' => [
+                    'test' => ['index_name' => 'test'],
+                ],
+                'managers' => [
+                    'test' => [
+                        'connection' => 'test2',
+                        'debug' => true,
+                        'readonly' => false,
+                        'mappings' => ['testBundle'],
                     ],
                 ],
             ],
-            $container
-        );
+        ];
 
         $expectedConnections = [
             'test' => [
@@ -56,6 +52,39 @@ class ElasticsearchExtensionTest extends \PHPUnit_Framework_TestCase
                 'settings' => [],
             ],
         ];
+
+        $expectedManagers = [
+            'test' => [
+                'connection' => 'test2',
+                'debug' => true,
+                'readonly' => false,
+                'mappings' => ['testBundle'],
+            ],
+        ];
+
+        $out[] = [
+            $parameters,
+            $expectedConnections,
+            $expectedManagers,
+        ];
+
+        $parameters = [
+            'elasticsearch' => [
+                'document_dir' => 'customDir',
+                'connections' => [
+                    'test' => ['index_name' => 'test'],
+                ],
+                'managers' => [
+                    'test' => [
+                        'connection' => 'test2',
+                        'debug' => false,
+                        'readonly' => false,
+                        'mappings' => ['testBundle'],
+                    ],
+                ],
+            ],
+        ];
+
         $expectedManagers = [
             'test' => [
                 'connection' => 'test2',
@@ -64,6 +93,75 @@ class ElasticsearchExtensionTest extends \PHPUnit_Framework_TestCase
                 'mappings' => ['testBundle'],
             ],
         ];
+
+        $out[] = [
+            $parameters,
+            $expectedConnections,
+            $expectedManagers,
+        ];
+
+        return $out;
+    }
+
+    /**
+     * Check if load adds parameters to container as expected.
+     *
+     * @param array $parameters
+     * @param array $expectedConnections
+     * @param array $expectedManagers
+     *
+     * @dataProvider getData
+     */
+    public function testLoad($parameters, $expectedConnections, $expectedManagers)
+    {
+        $container = new ContainerBuilder();
+        class_exists('testClass') ? : eval('class testClass {}');
+        $container->setParameter('kernel.bundles', ['testBundle' => 'testClass']);
+        $container->setParameter('kernel.cache_dir', '');
+        $container->setParameter('kernel.debug', true);
+        $extension = new ONGRElasticsearchExtension();
+        $extension->load(
+            $parameters,
+            $container
+        );
+
+        if ($parameters['elasticsearch']['managers']['test']['debug']) {
+            $reflection = new \ReflectionClass($this);
+            $dir = dirname($reflection->getFileName()) . DIRECTORY_SEPARATOR . 'customDir';
+
+            $handler = new Definition('ONGR\ElasticsearchBundle\Logger\Handler\CollectionHandler', []);
+            $logger = new Definition(
+                'Monolog\Logger',
+                [
+                    'tracer',
+                    [$handler],
+                ]
+            );
+
+            $collector = new Definition('ONGR\ElasticsearchBundle\DataCollector\ElasticsearchDataCollector');
+            $collector->addMethodCall('setManagers', [new Parameter('es.managers')]);
+            $collector->addMethodCall('addLogger', [new Reference('es.logger.trace')]);
+            $collector->addTag(
+                'data_collector',
+                [
+                    'template' => 'ONGRElasticsearchBundle:Profiler:profiler.html.twig',
+                    'id' => 'es',
+                ]
+            );
+
+            $this->assertEquals(
+                $collector,
+                $container->getDefinition('es.collector')
+            );
+            $this->assertEquals(
+                $logger,
+                $container->getDefinition('es.logger.trace')
+            );
+            $this->assertEquals(
+                new DirectoryResource($dir),
+                $container->getResources()[1]
+            );
+        }
 
         $this->assertEquals(
             $expectedConnections,
