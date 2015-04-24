271bda8a2772852d8bef06826bd4db5664b8b244
Mantas Simkus <mantas.simkus@ongr.io>
2015-04-24 11:34:49 +0300

    Functional PropertyTest was overwritten, adding missing tests for includeInAll

--- Tests/Functional/Annotation/PropertyTest.php
+++ Tests/Functional/Annotation/PropertyTest.php
@@ -62,4 +62,45 @@ class PropertyTest extends AbstractElasticsearchTestCase
         ];
         $this->assertEquals($expectedMapping, $result['ongr-esb-test']['mappings']['product']['stored']['mapping']);
     }
+
+    /**
+     * Data provider for testDocumentMappingWithIncludeInAll.
+     *
+     * @return array
+     */
+    public function getTestDocumentMappingWithIncludeInAllData()
+    {
+        $out = [];
+        // Case #0: should be included.
+        $out[] = ['field' => 'included_in_all', 'expected' => true];
+        // Case #1: should not be included.
+        $out[] = ['field' => 'excluded_from_all', 'expected' => false];
+        return $out;
+    }
+
+    /**
+     * Check if "include in all" option is set as expected.
+     *
+     * @param string $field
+     * @param bool $expected
+     *
+     * @dataProvider getTestDocumentMappingWithIncludeInAllData
+     */
+    public function testDocumentMappingWithIncludeInAll($field, $expected)
+    {
+        $manager = DelayedObjectWrapper::wrap($this->getManager());
+        $params = [
+            'index' => $manager->getConnection()->getIndexName(),
+            'type' => 'color',
+            'field' => $field,
+        ];
+        $result = $manager->getConnection()->getClient()->indices()->getFieldMapping($params);
+        $expectedMapping = [
+            $field => [
+                'include_in_all' => $expected,
+                'type' => 'string',
+            ],
+        ];
+        $this->assertEquals($expectedMapping, $result['ongr-esb-test']['mappings']['color'][$field]['mapping']);
+    }
 }
