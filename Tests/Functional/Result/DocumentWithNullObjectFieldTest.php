090b65f565cc36f55664fa7f08d1a871ab3ea723
ndinh215 <blackjackptit@yahoo.com>
2015-12-08 15:59:14 +0700

    Add the testcase for a returned document with null object fields

---
+++ Tests/Functional/Result/DocumentWithNullObjectFieldTest.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\Tests\Functional\Result;
+
+use ONGR\ElasticsearchDSL\Query\MatchAllQuery;
+use ONGR\ElasticsearchBundle\Service\Repository;
+use ONGR\ElasticsearchBundle\Test\AbstractElasticsearchTestCase;
+
+class DocumentNullObjectFieldTest extends AbstractElasticsearchTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDataArray()
+    {
+        return [
+            'default' => [
+                'product' => [
+                    [
+                        '_id' => 'Doc 1',
+                        'title' => 'Bar Product',
+                        'category' => null,
+                        'related_categories' => [
+                            [
+                                'title' => 'Acme',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Test the returned result with null object field
+     */
+    public function testResultWithNullObjectField()
+    {
+        $repo = $this->getManager()->getRepository('AcmeBarBundle:Product');
+        $match = new MatchAllQuery();
+        $search = $repo->createSearch()->addQuery($match);
+        $iterator = $repo->execute($search, Repository::RESULTS_OBJECT);
+
+        $this->assertInstanceOf('ONGR\ElasticsearchBundle\Result\DocumentIterator', $iterator);
+
+        foreach ($iterator as $document) {
+            $category = $document->category;
+
+            $this->assertInstanceOf('ONGR\ElasticsearchBundle\Result\ObjectIterator', $category);
+            $this->assertTrue($category->count() == 0);
+        }
+    }
+}
