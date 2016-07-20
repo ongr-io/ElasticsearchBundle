7b09933d02c87db4cfa9f4724a3bb11d1574ba9f
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-07-20 17:05:41 +0300

    added getter methods to inner hit value class

--- Result/InnerHitValue.php
+++ Result/InnerHitValue.php
@@ -10,10 +10,11 @@
  */
 
 namespace ONGR\ElasticsearchBundle\Result;
+
 use ONGR\ElasticsearchBundle\Service\Manager;
 
 /**
- * This is the class for plain aggregation result with nested aggregations support.
+ * This is the class for inner hit result with nested support.
  */
 class InnerHitValue
 {
@@ -28,8 +29,6 @@ class InnerHitValue
     private $manager;
 
     /**
-     * Constructor.
-     *
      * @param array   $rawData
      * @param Manager $manager
      */
@@ -37,5 +36,85 @@ class InnerHitValue
     {
         $this->rawData = $rawData;
         $this->manager = $manager;
+    }
+
+    /**
+     * Returns array of specific inner hit objects
+     *
+     * @param string $name
+     *
+     * @return object[]
+     */
+    public function getValue($name)
+    {
+        if (!isset($this->rawData[$name])) {
+            return null;
+        }
+
+        $hits = [];
+
+        foreach ($this->rawData[$name]['hits']['hits'] as $hit) {
+            if (isset($hit['_parent'])) {
+                $hits[] = $this->manager->getConverter()->convertToDocument($hit, $this->manager);
+            } else {
+                $fields = $hit;
+                $metadata = $this->manager
+                    ->getMetadataCollector()
+                    ->getMappings(
+                        $this->manager->getConfig()['mappings']
+                    )[$hit['_type']];
+
+                while (isset($fields['_nested'])) {
+                    $fields = $fields['_nested'];
+                    $metadata = $metadata['aliases'][$fields['field']];
+                }
+
+                $hits[] = $this->manager->getConverter()->assignArrayToObject(
+                    $hit['_source'],
+                    new $metadata['namespace'],
+                    $metadata['aliases']
+                );
+            }
+        };
+
+        return $hits;
+    }
+
+    /**
+     * Returns the count of inner hits for a specific hit
+     *
+     * @param string $name
+     *
+     * @return integer
+     */
+    public function getCount($name)
+    {
+        if (!isset($this->rawData[$name])) {
+            return null;
+        }
+
+        return $this->rawData[$name]['hits']['total'];
+    }
+
+    /**
+     * Returns inner hits for a specified inner hit
+     *
+     * @param string $name
+     *
+     * @return InnerHitValue[]|null
+     */
+    public function getInnerHits($name)
+    {
+        if (!isset($this->rawData[$name]['hits']['hits'][0]['inner_hits'])) {
+            return null;
+        }
+
+        $hits = [];
+
+        foreach ($this->rawData[$name]['hits']['hits'] as $hit) {
+            $hits[] = new self($hit['inner_hits'], $this->manager);
+        }
+
+        return $hits;
     }
 }
