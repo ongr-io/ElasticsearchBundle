50a59b377371c70965423eb9880fe4f3360f3016
Mantas Marcinkevičius <marc.mantas@gmail.com>
2017-03-16 09:25:20 +0200

    updated the entity products

--- Tests/app/fixture/TestBundle/Entity/Product.php
+++ Tests/app/fixture/TestBundle/Entity/Product.php
@@ -12,13 +12,37 @@
 namespace ONGR\ElasticsearchBundle\Tests\app\fixture\TestBundle\Entity;
 
 use ONGR\ElasticsearchBundle\Annotation as ES;
-use ONGR\ElasticsearchBundle\Tests\app\fixture\TestBundle\Document\Product as BaseProduct;
+use ONGR\ElasticsearchBundle\Collection\Collection;
 
 /**
  * Product document for testing.
  *
  * @ES\Document()
  */
-class Product extends BaseProduct
+class Product
 {
+    /**
+     * @var string
+     *
+     * @ES\Id()
+     */
+    public $id;
+
+    /**
+     * @var string|array
+     * @ES\Property(type="text", name="description")
+     */
+    public $description;
+
+    /**
+     * @var Variant[]
+     *
+     * @ES\Embedded(class="TestBundle:Variant", multiple=true)
+     */
+    public $variants;
+
+    public function __construct()
+    {
+        $this->variants = new Collection();
+    }
 }
