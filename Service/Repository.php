02e29714c93d0df839f5e4182fdb07d6a6491ec8
Simonas Šerlinskas <saimaz@users.noreply.github.com>
2017-03-30 09:15:53 +0300

    added default result structure for findbyids (#771)

--- Service/Repository.php
+++ Service/Repository.php
@@ -95,7 +95,7 @@ class Repository
     /**
      * Returns documents by a set of ids
      *
-     * @param array  $ids
+     * @param array $ids
      *
      * @return DocumentIterator The objects.
      */
@@ -115,7 +115,12 @@ class Repository
 
         $mgetResponse = $manager->getClient()->mget($args);
 
-        $return = [];
+        $return = [
+            'hits' => [
+                'hits' => [],
+                'total' => 0,
+            ]
+        ];
 
         foreach ($mgetResponse['docs'] as $item) {
             if ($item['found']) {
