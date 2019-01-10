d27b57e5caa9a90d5bd5bb68ffd3dad1ae277aac
Kifir22 <kifir22@gmail.com>
2019-01-10 20:21:30 +0300

    Events::PRE_PERSIST work with object before bulk (#866)

---
+++ Event/PrePersistEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class PrePersistEvent extends Event
+{
+    /**
+     * @var object
+     */
+    private $document;
+
+    /**
+     * PrePersistEvent constructor.
+     * @param $document
+     */
+    public function __construct($document)
+    {
+        $this->document = $document;
+    }
+
+    /**
+     * @return object
+     */
+    public function getDocument()
+    {
+        return $this->document;
+    }
+
+    /**
+     * @param object $document
+     */
+    public function setDocument($document)
+    {
+        $this->document = $document;
+    }
+}
