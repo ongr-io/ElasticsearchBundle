df8fccb7987b217141ec08cab11f2c50f36763d9
Peter Svistunov <svistunov.p@i20.biz>
2019-09-19 12:54:19 +0800

    Fix symfony 4.2 command related deprecations

--- Command/AbstractManagerAwareCommand.php
+++ Command/AbstractManagerAwareCommand.php
@@ -14,12 +14,52 @@ namespace ONGR\ElasticsearchBundle\Command;
 use ONGR\ElasticsearchBundle\Service\Manager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * AbstractElasticsearchCommand class.
  */
 abstract class AbstractManagerAwareCommand extends Command
 {
+
+    /**
+     * @var ContainerInterface|null
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        parent::__construct();
+    }
+
+    /**
+     * @return ContainerInterface
+     *
+     * @throws \LogicException
+     */
+    protected function getContainer()
+    {
+        if (null === $this->container) {
+            $application = $this->getApplication();
+            if (null === $application) {
+                throw new \LogicException('The container cannot be retrieved as the application instance is not yet set.');
+            }
+
+            $this->container = $application->getKernel()->getContainer();
+        }
+
+        return $this->container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * {@inheritdoc}
      */
