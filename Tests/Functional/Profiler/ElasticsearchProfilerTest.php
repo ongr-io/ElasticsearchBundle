8cce345e9e2975cd69f8813482c0f5a01d4d4c4c
Mantas Marcinkevicius <mantas.marcinkevicius@nfq.lt>
2019-09-26 17:40:30 +0300

    fixed failing profiler test

--- Tests/Functional/Profiler/ElasticsearchProfilerTest.php
+++ Tests/Functional/Profiler/ElasticsearchProfilerTest.php
@@ -117,7 +117,7 @@ class ElasticsearchProfilerTest extends AbstractElasticsearchTestCase
         $this->assertEquals(
             [
                 'body' => json_encode($search->toArray()),
-                'method' => 'GET',
+                'method' => 'POST',
                 'httpParameters' => [],
                 'scheme' => 'http',
                 'port' => 9200,
