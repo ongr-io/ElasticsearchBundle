7e3354d77a57088b2196bd3c3f7af03127e9a7a9
Toni Rudolf <toni.rudolf@gmail.com>
2020-02-11 10:50:48 +0100

    [POC] Implemtend working config with multiple indexes per document class

---
+++ Exception/DocumentIndexParserException.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\Exception;
+
+class DocumentIndexParserException extends DocumentParserException
+{
+}
